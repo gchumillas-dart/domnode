d581a9d0f2f5179254e40f43d14e0da2ab3e4d63
soloproyectos <gonzalo.chumillas@gmail.com>
2016-06-22 17:12:15 +0200

    update example

--- web/main.dart
+++ web/main.dart
@@ -1,45 +1,17 @@
 import 'package:domnode/core.dart';
 
 void main() {
-  DomNode span = new DomNode('span', text: 'This is a text')
-    ..setAttr('data-test', 'lol')
-    ..setHtml('<custom-component></custom-component>')
-    ..append('<lalala></lalala>')
-    ..prepend('<ups>jejeje</ups>');
-  span.setData('test', ['hello', 101]);
-  print(span.getData('test'));
-  /*
-  Iterable<DomNode> node = $('root', callback: (DomNode target) {
-    // apends a new node with childNodes
-    target.append($('user', callback: (DomNode target) {
-      target.append($('first-name', text: 'James'));
-      target.append($('last-name', text: 'Bond'));
-      target.append($('age', text: 158));
-      target.append($('bio', html: 'My name is Bond, <em>James Bond</em>'));
-    }));
-
-    // appends more items
-    target.append($('item', attributes: {'id': 101, 'title': 'Item 1'}));
-    target.append($('item', attributes: {'id': 102, 'title': 'Item 2'}));
-    target.append($('item', attributes: {'id': 103, 'title': 'Item 3'}));
-
-    // appends raw content
-    target.append('<summary>Look at my horse, my horse is amazing</summary>');
-  });
-  print(node);*/
-  /*
   // accessing a single element
-  domQuery('h1')
-    ..text('Changing title')
-    ..css('border', '1px solid black');
+  $('h1')
+    ..setText('Changing title')
+    ..setCssAttr('border', '1px solid black');
 
   // accessing multiple elements
-  DomNode paragraphs = domQuery('p');
+  Iterable<DomNode> paragraphs = $('p');
   paragraphs.forEach((DomNode item) {
-    item.html('<strong>Paragraph</strong>');
+    item.setHtml('<strong>Paragraph</strong>');
   });
 
   // appends content
-  domQuery('p:last-child')
-      .append('<span>content added to the last paragraph.</span>');*/
+  $('p:last-child').append('<span>content added to the last paragraph.</span>');
 }
