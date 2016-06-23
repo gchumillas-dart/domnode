59d05d190dde9259eac3213ef70399151ab560be
soloproyectos <gonzalo.chumillas@gmail.com>
2016-06-23 21:26:11 +0200

    update example

--- web/main.dart
+++ web/main.dart
@@ -1,10 +1,17 @@
 import 'package:domnode/core.dart';
 
 void main() {
+  DomNode node =
+      new DomNode('a', attrs: {'href': '#'}, html: '<em>Some & text</em>');
+  $('body').append(node);
+  print(node.getText());
+
   // accessing a single element
   $('h1')
+    ..setAttr('title', 'This is my title')
     ..setText('Changing title')
     ..setCssAttr('border', '1px solid black');
+  $('h1').setText('title');
 
   // accessing multiple elements
   Iterable<DomNode> paragraphs = $('p');
@@ -13,5 +20,6 @@ void main() {
   });
 
   // appends content
-  $('p:last-child').append('<span>content added to the last paragraph.</span>');
+  $('p:last-child')
+      .append('<br><span>content added to the last paragraph.</span>');
 }
