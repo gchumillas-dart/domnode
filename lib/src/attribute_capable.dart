7c494dc0e517b3f851e54e02e86b93e0849661fb
soloproyectos <gonzalo.chumillas@gmail.com>
2016-06-23 20:22:23 +0200

    recode

--- lib/src/attribute_capable.dart
+++ lib/src/attribute_capable.dart
@@ -10,19 +10,8 @@ abstract class AttributeCapable {
     return elements.length > 0 ? elements[0].getAttribute(name) != null : false;
   }
 
-  // TODO: it should return a null element if the attribute does no exist
   String getAttr(String name) {
-    String ret = '';
-
-    if (elements.length > 0) {
-      String attr = elements[0].getAttribute(name);
-
-      if (attr != null) {
-        ret = attr;
-      }
-    }
-
-    return ret;
+    return elements.length > 0 ? elements[0].getAttribute(name) : null;
   }
 
   void setAttr(String name, Object value) {
