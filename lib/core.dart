63f99b873fde1ea68dd7ca6efcaf431ca45248b6
soloproyectos <gonzalo.chumillas@gmail.com>
2016-07-02 20:30:13 +0200

    extend $ function

--- lib/core.dart
+++ lib/core.dart
@@ -19,10 +19,46 @@ part 'src/event_capable.dart';
 part 'src/metrics_capable.dart';
 part 'src/null_tree_sanitizer.dart';
 
-/**
- * Searches nodes by CSS selectors.
- */
-DomNode $(String cssSelectors) {
-  DomNode node = new DomNode.fromDocument(document);
-  return node.query(cssSelectors);
+/// This function has several uses:
+///
+/// * Searches for elements:
+/// DomNode node = $('css selectors')
+///
+/// * Creates a node from a valid XML document
+/// DomNode node = $('<span title="My SPAN">Some text...</span>');
+///
+/// * Creates a node from an element:
+/// Element elem = document.querySelector('p');
+/// DomNode node = $(elem);
+///
+/// * Creates a node from a document:
+/// DomNode node = $(document);
+///
+/// * Redundant but programmatically useful:
+/// DomNode target = new DomNode('span');
+/// node = $(target);
+/// assert(node == target);
+DomNode $(dynamic target) {
+  DomNode ret;
+
+  if (target is String) {
+    Parser parser = new xml.XmlParserDefinition().build();
+    Result result = parser.parse(target);
+    if (result.isSuccess) {
+      // xml document
+      ret = new DomNode.fromString(target);
+    } else {
+      // css selectors
+      DomNode node = new DomNode.fromDocument(document);
+      ret = node.query(target);
+    }
+  } else if (target is Element) {
+    ret = new DomNode.fromElement(target);
+  } else if (target is Document) {
+    ret = new DomNode.fromDocument(target);
+  } else if (target is DomNode) {
+    ret = target;
+  }
+
+  return ret;
 }
