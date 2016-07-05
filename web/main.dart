6e041f9f8a6a7b18b51377543ed25c18b1debd9e
soloproyectos <gonzalo.chumillas@gmail.com>
2016-07-05 23:15:29 +0200

    update example

--- web/main.dart
+++ web/main.dart
@@ -2,18 +2,18 @@ import 'package:domnode/core.dart';
 
 void main() {
   // accessing a single element
-  q('h1')
-    ..setAttr('title', 'This is my title')
-    ..text = 'Changing title'
-    ..setCssAttr('border', '1px solid black');
-  q('h1').text = 'title';
+  query('h1')
+    ..attr['title'] = 'This is my title'
+    ..css['border'] = '1px solid black'
+    ..text = 'Changing title';
 
   // accessing multiple elements
-  Iterable<DomNode> paragraphs = q('p');
+  Iterable<DomNode> paragraphs = query('p');
   paragraphs.forEach((DomNode item) {
     item.html = '<strong>Paragraph</strong>';
   });
 
   // appends content
-  q('p:last-child').append('<br><em>content added to the last paragraph.</em>');
+  query('p:last-child')
+      .append('<br><em>content added to the last paragraph.</em>');
 }
